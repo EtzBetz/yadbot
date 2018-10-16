d88adb5a9da26250d1776f410e9de7a3976e3cc8
Raphael Betz <raphaelbetz@gmail.com>
2018-10-16 12:54:40 +0200

    update Config-Blueprint to reflect changes to Config.py

--- classes/Config-Blueprint.py
+++ classes/Config-Blueprint.py
@@ -1,4 +1,3 @@
-# RENAME FILE TO "Config.py" AFTER SETTING IT UP #
 class Config:
 
     token = ""  # token of the bot user
@@ -14,17 +13,22 @@ class Config:
     new_user_guild_id = 133333333333333337  # the server id on which it will assign new users a specific role
     new_user_role_id = 133333333333333337  # the role id which will be assigned to new users on the specified server
 
-    faulty_member_guild_id = 133333333333333337  # the server id of iQ, to get the latest bad member
-    faulty_commands_channel_id = 133333333333333337  # channel id of the channel to write commands into
-    faulty_member_role_id = 133333333333333337  # the role id for the current weeks faulty member
-    old_faulty_member_role_id = 133333333333333337  # the role id for the last weeks faulty member
+    guilty_member_guild_id = 133333333333333337  # the server id of iQ, to get the latest bad member
+    guilty_commands_channel_id = 133333333333333337  # channel id of the channel to write commands into
+    guilty_news_channel_id = 133333333333333337  # channel id of the channel where guilty news are announced to
+    guilty_member_role_id = 133333333333333337  # the role id for the current weeks faulty member
+    old_guilty_member_role_id = 133333333333333337  # the role id for the last weeks faulty member
 
     bot_id = 133333333333333337  # userId of the bot
     admin_id = 133333333333333337  # userId of the bot admin
     iq_leaders_id = {  # userId of the iq leaders (usage in !schuld confirm)
-        133333333333333337,  # some userId
+        133333333333333337,  # person1
+        # 133333333333333337,  # person2
         # admin_id,
     }
 
     ping_pong_loop = 0  # if set true, the bot will trigger itself with ping and pong commands
     airhorn_stay_afterwards = 0  # if set true, the bot will stay in voice after the airhorn was played
+
+    support_skip_all = 0  # if set true, the bot will not send any support messages to bot-admin
+    support_skip_player_missing_in_db = 0  # if set true, the bot will not send support messages regarding missing players in db
